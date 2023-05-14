af027d294ee0389dc12d18678eef47c868de7321
mrprotocoll <lekanvgbg@gmail.com>
2023-05-14 14:33:28 +0100

    add user controller

--- app/controllers/registrations_controller.rb
+++
@@ -1,2 +0,0 @@
-class RegistrationsController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/sign-up", to: "users#new"
+
+  resources :users
+
   # Defines the root path route ("/")
   root "main#index"
 end
