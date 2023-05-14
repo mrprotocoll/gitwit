c1a0cc30eb0e441a487c53af45c82896cef59c3e
mrprotocoll <lekanvgbg@gmail.com>
2023-05-14 14:09:00 +0100

    Add registration controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
     
-    validates email, presence: true
+    validates email, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "email must be a valid email address"}
 end
--- db/migrate/20230514121231_create_users.rb
+++ db/migrate/20230514121231_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :email
+      t.string :email, null: false
       t.string :password_digest
 
       t.timestamps
