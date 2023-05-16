e49ffb65379f5bdffb50205e930e1bdc62b4bff1
mrprotocoll <lekanvgbg@gmail.com>
2023-05-16 07:37:30 +0100

    Fixed Linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,11 @@ class ApplicationController < ActionController::Base
     Current.user = User.find_by(id: session[:user_id])
   end
 
-  def is_user_logged_in?
+  def user_is_logged_in?
     Current.user.nil?
   end
 
   def require_login!
-    redirect_to login_path, alert: "Authentication Required" if is_user_logged_in?
+    redirect_to login_path, alert: 'Authentication Required' if user_is_logged_in?
   end
 end
--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -1,21 +1,20 @@
 class PasswordController < ApplicationController
-    before_action :require_login!
+  before_action :require_login!
 
-    def edit
+  def edit; end
+
+  def update
+    user = Current.user.update(update_params)
+    if user
+      redirect_to profile_path, notice: 'Password changed successfully'
+    else
+      render :edit
     end
+  end
 
-    def update
-        user = Current.user.update(update_params)
-        if user
-            redirect_to profile_path, notice: 'Password changed successfully'
-        else
-            render :edit
-        end
-    end
+  private
 
-    private
-
-    def update_params
-        params.require(:user).permit(:password, :password_confirmation)
-    end
+  def update_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
