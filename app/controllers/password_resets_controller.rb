ac8d09605462b887357b93bb1f2e1bc9f80b745e
mrprotocoll <lekanvgbg@gmail.com>
2023-05-19 05:09:41 +0100

    fix password reset route

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,5 +3,10 @@ class PasswordResetsController < ApplicationController
     end
 
     def create
+        @user = User.find(email: params[:email])
+        if @user.present?
+        end
+
+        redirect_to login_path, notice: "Reset your password using the link sent to your email"
     end
 end
