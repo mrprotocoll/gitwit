eb4eb7023c16001c94942141e5c01e59085a221c
mrprotocoll <lekanvgbg@gmail.com>
2023-05-23 06:46:27 +0100

    update twitter account model to connect twitter account to the signed in user

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,22 @@
 class OmniauthCallbacksController < ApplicationController
   def twitter
-    render plain: 'Success!'
+    if(Current.user.present?)
+        Current.user.twitter_accounts.create(
+            name: auth.info.name,
+            username: auth.info.nickname,
+            image: auth.info.image,
+            token: auth.credentials.token,
+            secret: auth.credentials.secret
+        )
+    else
+        redirect_to root_path, alert: "Oops you have to sign in" and return
+        
+    end
+
+    redirect_to root_path, notice: "Twitter account successfully connected"
+  end
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -1,3 +1,5 @@
 class TwitterAccount < ApplicationRecord
   belongs_to :user
+
+  validates :username, uniqueness: true
 end
