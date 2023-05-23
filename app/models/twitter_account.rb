0f47691c5c136e6741295c40c15b61be477d203a
mrprotocoll <lekanvgbg@gmail.com>
2023-05-23 06:25:45 +0100

    Add twitter account model and create association with users table

---
+++ app/models/twitter_account.rb
@@ -0,0 +1,3 @@
+class TwitterAccount < ApplicationRecord
+  belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :twitter_accounts
   has_secure_password
 
   validates :email, presence: true,
