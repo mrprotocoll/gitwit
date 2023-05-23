cf4fce2924ce640e33eeaeefd7233d30e9aef75d
mrprotocoll <lekanvgbg@gmail.com>
2023-05-23 11:56:53 +0100

    Fixed linters

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,19 +1,14 @@
 class OmniauthCallbacksController < ApplicationController
   def twitter
-    if(Current.user.present?)
-        Current.user.twitter_accounts.create(
-            name: auth.info.name,
-            username: auth.info.nickname,
-            image: auth.info.image,
-            token: auth.credentials.token,
-            secret: auth.credentials.secret
-        )
-    else
-        redirect_to root_path, alert: "Oops you have to sign in" and return
-        
-    end
+    redirect_to root_path, alert: 'Oops you have to sign in' and return unless Current.user.twitter_accounts.create(
+      name: auth.info.name,
+      username: auth.info.nickname,
+      image: auth.info.image,
+      token: auth.credentials.token,
+      secret: auth.credentials.secret
+    )
 
-    redirect_to twitter_accounts_path, notice: "Twitter account successfully connected"
+    redirect_to twitter_accounts_path, notice: 'Twitter account successfully connected'
   end
 
   def auth
--- test/models/twitter_account_test.rb
+++ test/models/twitter_account_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class TwitterAccountTest < ActiveSupport::TestCase
   # test "the truth" do
