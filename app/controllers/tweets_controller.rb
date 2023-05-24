877605f9976ce119cc11808f4acd636e823f83b5
mrprotocoll <lekanvgbg@gmail.com>
2023-05-24 21:08:54 +0100

    Add tweet edit form and action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
     before_action :require_login!
+    before_action :set_tweet, only: [:edit, :update, :destroy]
 
     def index
         @tweets = Current.user.tweets
@@ -19,9 +20,19 @@ class TweetsController < ApplicationController
         end
     end
 
+    def edit
+    end
+
+    def update
+    end
+
     private
 
     def tweet_params
         params.require(:tweet).permit(:twitter_account_id, :body, :published_at)
+    end
+
+    def set_tweet
+        @tweet = Current.user.tweets.find(params[:id])
     end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -9,4 +9,8 @@ class Tweet < ApplicationRecord
     after_initialize do
         self.published_at ||= 24.hours.from_now
     end
+
+    def published?
+        tweet_id?
+    end
 end
