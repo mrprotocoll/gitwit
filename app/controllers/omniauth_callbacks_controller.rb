78afe86b226972b6549b8def271d5d5f3d155cd9
mrprotocoll <lekanvgbg@gmail.com>
2023-06-04 17:17:59 +0100

    Add github auth callback action

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -11,6 +11,19 @@ class OmniauthCallbacksController < ApplicationController
     redirect_to twitter_accounts_path, notice: 'Twitter account successfully connected'
   end
 
+  def github
+    p auth
+    redirect_to root_path, alert: 'Oops you have to sign in' and return unless Current.user.github_accounts.create(
+      name: auth.info.name,
+      username: auth.info.nickname,
+      image: auth.info.image,
+      token: auth.credentials.token,
+      secret: auth.credentials.secret
+    )
+
+    redirect_to github_accounts_path, notice: 'Github account successfully connected'
+  end
+
   def auth
     request.env['omniauth.auth']
   end
