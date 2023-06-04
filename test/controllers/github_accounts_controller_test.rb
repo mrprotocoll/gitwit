3d1a97b3d9981014b49d7ca94146b5e37dab106a
mrprotocoll <lekanvgbg@gmail.com>
2023-06-04 15:48:28 +0100

    Fix linters

--- test/controllers/github_accounts_controller_test.rb
+++ test/controllers/github_accounts_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class GithubAccountsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
--- test/controllers/github_controller_test.rb
+++ test/controllers/github_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class GithubControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
